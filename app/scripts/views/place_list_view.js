89a70b07a0f93a3779b797d1b59323cffa18c45d
egjimenezg <egjimenezg@gmail.com>
2015-11-15 13:45:03 -0600

    coding #4 adding script for render the data in the view

---
+++ app/scripts/views/place_list_view.js
@@ -0,0 +1,15 @@
+var PlaceListView = (function(){
+  var settings = {
+    renderDiv:'#applicationDiv'
+  };
+
+  var render = function(data){
+    var html = FriendlyDolphin.templates.placeList(data);
+    $(settings.renderDiv).html(html); 
+  };
+
+  return{
+    render:render
+  };
+
+}());
