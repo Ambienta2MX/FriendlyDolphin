79f534d2737676faf183d236cfa381746fa6b67c
egjimenezg <egjimenezg@gmail.com>
2015-11-15 13:18:22 -0600

    coding #4 adding model for the places

---
+++ app/scripts/models/place.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var Place = {
+  city:'',
+  extraInfo:[],
+  fullName:'',
+  height:'',
+  itrf_coordinates:[],
+  location:null,
+  nad27_coordinates:[],
+  sexagesimal_coordinates:[],
+  state:'',
+  town:'',
+  zipCode:'',
+
+  create:function(data){
+    return $.extend({},this,data);
+  },
+
+  deserialize: function(data){
+    var _self = Place.create({});
+
+    Object.keys(_self).forEach(function(key){
+      if(typeof _self[key] != 'function'){
+        _self[key] = data.place[key];
+      }
+    });
+    
+    return _self;
+  },
+
+  list:function(params){
+    return new RSVP.Promise(function(resolve,reject){
+      $.ajax({
+        url:params.url,
+        type:'GET',
+        data:params.data,
+        datatype:'json',
+        contentType:'application/json; charset=utf-8' 
+      })
+      .done(function(response){
+        var model = {places:[]};
+
+        response.forEach(function(item){
+          var place = Place.deserialize({place:item});
+          model.places.push(place);
+        });
+        
+        resolve(model);
+      }).
+      fail(function(response){
+        reject(response);
+      });
+    });
+  }
+
+};
