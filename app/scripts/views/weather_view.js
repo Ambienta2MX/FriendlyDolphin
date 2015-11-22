bbfa3bcc0de12649efc5ef4f476bdd62b899771a
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-11-22 17:07:15 -0600

    #5, generating DF map configuratioN

---
+++ app/scripts/views/weather_view.js
@@ -0,0 +1,15 @@
+module.exports = (function(){
+  var settings = {
+    renderDiv:'#applicationDiv'
+  };
+
+  var render = function(data){
+    var html = FriendlyDolphin.templates.weather();
+    $(settings.renderDiv).html(html); 
+  };
+
+  return{
+    render:render
+  };
+
+}());
